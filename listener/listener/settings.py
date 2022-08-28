2e508d29fa0aecaca81e1f5dd82a0e31a50a6a3a
Joe Weiss <joe.weiss@gmail.com>
2022-08-28 14:31:07 -0400

    Add basic nginx/uwsgi setup

--- listener/listener/settings.py
+++ listener/listener/settings.py
@@ -35,7 +35,7 @@ SECRET_KEY = os.environ.get(
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["127.0.0.1", "localhost", "pi400-64bit.local"]
 
 
 # Application definition
@@ -137,6 +137,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = "/static/"
+STATIC_ROOT = os.path.join(BASE_DIR, "static/")
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
