86c4beef440ab51dffa4a5eb4c6d87a38f08801b
Joe Weiss <joe.weiss@gmail.com>
2022-09-01 09:22:33 -0400

    Add recording example MacOS, sox and pysox

---
+++ script_examples/audio_recording_macos.py
@@ -0,0 +1,30 @@
+# Requires sox and pysox: install with:
+#  brew install sox
+#  pip install sox
+
+import sox
+from datetime import datetime
+
+
+def main():
+
+    duration_secs = 15
+    output_directory = "audio_inbox"
+
+    while True:
+        filename = datetime.now().strftime("%Y-%m-%d-%H:%M:%S.wav")
+        print(f"{output_directory}/{filename}")
+        args = [
+            "-d",
+            "-t",
+            "wav",
+            f"{output_directory}/{filename}",
+            "trim",
+            "0",
+            f"{duration_secs}",
+        ]
+        sox.core.sox(args)
+
+
+if __name__ == "__main__":
+    main()
